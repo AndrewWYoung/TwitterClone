2ee7e056a5f4b856ab2c8b536b07e9f1d0b678e1
Devin Riegle <riegledevin@gmail.com>
2017-04-16 00:45:14 -0400

    Updated tweet user information to match currentUser & fixed delete functionality to work only on user that created the tweet

--- routes/index.js
+++ routes/index.js
@@ -26,18 +26,24 @@ router.get("/tweets", function(req, res) {
 
 // CREATE - add new tweet to DB
 router.post("/tweets", function(req, res) {
-    const name = req.body.name;
-    const image = twitterIcon;
-    const tweet = req.body.tweet;
-    const newTweet = { name: name, image: image, tweet: tweet };
-
-    Tweet.create(newTweet, function(err) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.redirect("/tweets");
-        }
-    });
+    const newTweet = { tweet: req.body.tweet };
+    // Check if currentUser is loggedin
+    if(res.locals.currentUser){
+       Tweet.create(newTweet, function(err, tweet) {
+            if (err) {
+                console.log(err);
+            } else {
+                // Add user information to tweet (id, username)
+                tweet.user.id = res.locals.currentUser._id;
+                tweet.user.username = res.locals.currentUser.username;
+                tweet.save();
+                res.redirect("/tweets");
+            }
+        }); 
+    // if NO currentUser logged in, redirect to /tweets
+    } else {
+        res.redirect("/tweets");
+    }
 });
 
 // DELETE - delete tweet from DB using MongoDB ID
@@ -45,14 +51,22 @@ router.delete("/tweets/:id", function(req, res){
     // Find the Tweet in the Database and remove it
     // /tweets/:id is the route that we are using to delete tweets
     // You get the :id from the route using "req.params.id"
-    Tweet.findByIdAndRemove(req.params.id, function(err){
-        if(err){
-            console.log(err);
-            res.redirect("/tweets");
-        } else {
-            res.redirect("/tweets");
-        }
-    });
+    // Check if there is a user logged in
+    if(res.locals.currentUser){
+        Tweet.findById(req.params.id, function(err, tweet){
+            if(err){
+                console.log(err);
+                res.redirect("/tweets");
+            } else {
+                // Check to make sure the currentUser trying to delete tweet is the creator
+                if(res.locals.currentUser.username == tweet.user.username){
+                    // remove tweet from DB & redirect
+                    tweet.remove();
+                    res.redirect("/tweets");
+                }
+            }
+        });
+    }
 });
 
 // INCLUDE USER ROUTES from user.js file
--- seeds.js
+++ seeds.js
@@ -1,56 +1,49 @@
 const Tweet = require("./models/tweet");
-const Comment = require("./models/comment");
+// defaultTwitterIcon = https://img.clipartfest.com/672f88933a5add7f407647d3ac640baf_circle-twitter-icon-twitter-icon-clipart_512-512.png
 
 var data = [
     {
-        name: "Cloud's Rest",
-        image: "https://img.clipartfest.com/672f88933a5add7f407647d3ac640baf_circle-twitter-icon-twitter-icon-clipart_512-512.png",
-        tweet: "blah blah blah"
+        user: {username: "admin"},
+        tweet: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.",
+        meta: {likes: 20, retweets: 5, replies: 2}
     },
     {
-        name: "Desert Mesa",
-        image: "https://img.clipartfest.com/672f88933a5add7f407647d3ac640baf_circle-twitter-icon-twitter-icon-clipart_512-512.png",
-        tweet: "blah blah blah"
+        user: {username: "admin"},
+        tweet: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.",
+        meta: {likes: 10}
     },
     {
-        name: "Canyon Floor",
-        image: "https://img.clipartfest.com/672f88933a5add7f407647d3ac640baf_circle-twitter-icon-twitter-icon-clipart_512-512.png",
-        tweet: "blah blah blah"
+        user: {username: "default"},
+        tweet: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.",
+        meta: {retweets: 25}
     }
 ];
 
 function seedDB() {
-    //Remove all tweets
-    Tweet.remove({}, function(err) {
-        if (err) {
-            console.log(err);
-        }
-        console.log("removed tweets!");
-        //add a few tweets
-        data.forEach(function(seed) {
-            Tweet.create(seed, function(err, tweet) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("added a tweet");
-                    //create a comment
-                    Comment.create({
-                        text: "This place is great",
-                        author: "Homer"
-                    }, function(err, comment) {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            tweet.comments.push(comment);
-                            tweet.save();
-                            console.log("Created new comment");
-                        }
-                    });
-                }
-            });
+    removeAllTweets();
+    createTweets();
+}
+
+function createTweets(){
+    data.forEach(function(seed){
+        Tweet.create(seed, function(err, tweet){
+            if(err){
+                console.log(err);
+            } else {
+                console.log("Added Tweet by " + tweet.user.username);
+            }
         });
     });
-    //add a few comments
+}
+
+function removeAllTweets(){
+    Tweet.remove({}, function(err){
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("Remove all tweets");
+        }
+    });
 }
 
 module.exports = seedDB;
