035797a31aab0e4763fe6c812cd8517f2c67a756
Devin Riegle <riegledevin@gmail.com>
2017-04-16 01:12:32 -0400

    Delete comment model (NOT needed. There are replies/retweets. No comments) Added likes,retweets,replies to /tweets page

--- models/comment.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-
-var commentSchema = mongoose.Schema({
-    text: String,
-    author: String
-});
-
-module.exports = mongoose.model("Comment", commentSchema);
