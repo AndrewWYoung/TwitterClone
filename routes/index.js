9be9e5f0e6a3295cd047152453085904706a01fe
Andrew <andrew.young.w@gmail.com>
2017-04-17 09:34:23 -0400

    modified for a fuzzy search. queries tweets with specified keywords.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 
 const router = require("express").Router();
 const Tweet = require("../models/tweet");
-const User  = require("../models/user");
+const User = require("../models/user");
 const userRoutes = require("./user.js");
 
 
@@ -13,22 +13,40 @@ router.get("/", function(req, res) {
 
 // INDEX - show all tweets
 router.get("/tweets", function(req, res) {
-    Tweet.find({}, function(err, allTweets) {
-        if (err) {
-            console.log(err);
-        } else {
-            // Sort tweets by date
-            allTweets.sort(function(a, b) {return (a.date < b.date) ? 1 : ((a.date > b.date) ? -1 : 0);});
-            res.render("index", { tweets: allTweets });
-        }
-    });
+    //
+    if (req.query.search) {
+        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+        /* searches tweets based on search query */
+        Tweet.find({ tweet: regex }, function(err, allTweets) {
+            console.log("SEARCH TEST");
+            console.log(regex);
+            if (err) {
+                console.log(err);
+            } else {
+                // Sort tweets by date
+                allTweets.sort(function(a, b) { return (a.date < b.date) ? 1 : ((a.date > b.date) ? -1 : 0); });
+                res.render("index", { tweets: allTweets });
+            }
+        });
+    } else {
+        //
+        Tweet.find({}, function(err, allTweets) {
+            if (err) {
+                console.log(err);
+            } else {
+                // Sort tweets by date
+                allTweets.sort(function(a, b) { return (a.date < b.date) ? 1 : ((a.date > b.date) ? -1 : 0); });
+                res.render("index", { tweets: allTweets });
+            }
+        });
+    }
 });
 
 // CREATE - add new tweet to DB
 router.post("/tweets", function(req, res) {
     const newTweet = { tweet: req.body.tweet };
     // Check if currentUser is loggedin
-    if(res.locals.currentUser){
+    if (res.locals.currentUser) {
         Tweet.create(newTweet, function(err, tweet) {
             if (err) {
                 console.log(err);
@@ -39,27 +57,27 @@ router.post("/tweets", function(req, res) {
                 tweet.save();
                 res.redirect("/tweets");
             }
-        }); 
-    // if NO currentUser logged in, redirect to /tweets
+        });
+        // if NO currentUser logged in, redirect to /tweets
     } else {
         res.redirect("/tweets");
     }
 });
 
 // DELETE - delete tweet from DB using MongoDB ID
-router.delete("/tweets/:id", function(req, res){
+router.delete("/tweets/:id", function(req, res) {
     // Find the Tweet in the Database and remove it
     // /tweets/:id is the route that we are using to delete tweets
     // You get the :id from the route using "req.params.id"
     // Check if there is a user logged in
-    if(res.locals.currentUser){
-        Tweet.findById(req.params.id, function(err, tweet){
-            if(err){
+    if (res.locals.currentUser) {
+        Tweet.findById(req.params.id, function(err, tweet) {
+            if (err) {
                 console.log(err);
                 res.redirect("/tweets");
             } else {
                 // Check to make sure the currentUser trying to delete tweet is the creator
-                if(res.locals.currentUser.username == tweet.user.username){
+                if (res.locals.currentUser.username == tweet.user.username) {
                     // remove tweet from DB & redirect
                     tweet.remove();
                     res.redirect("/tweets");
@@ -74,16 +92,20 @@ router.delete("/tweets/:id", function(req, res){
 // INCLUDE USER ROUTES from user.js file
 router.use("/", userRoutes);
 
-router.get("/:username", function(req, res){
-    User.findOne({username: req.params.username}, function(err, user){
-        if(err){
+router.get("/:username", function(req, res) {
+    User.findOne({ username: req.params.username }, function(err, user) {
+        if (err) {
             console.log(err);
             res.redirect("/tweets");
         } else {
-            res.render("profile.ejs", {profile: user});
+            res.render("profile.ejs", { profile: user });
         }
     });
 });
 
+
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
 
 module.exports = router;
